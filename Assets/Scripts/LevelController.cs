79e7668ff23ea19ee3d10a678fa1a7769b3346f7
Karulin Grigoriy <g.karulin@carx-tech.com>
2025-05-07 17:54:47 +0300

    гольф финал

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -1,18 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class NewBehaviourScript : MonoBehaviour
+namespace Golf
 {
-    // Start is called before the first frame update
-    void Start()
+    public class LevelController : MonoBehaviour
     {
-        
+        public SpawnerStone spawner;
+        public float delayMax = 2f;
+        public float delayMin = 0.5f;
+        public float delayStep = 0.1f;
+        private float m_delay = 0.5f;
+        private float m_lastSpawnedTime = 0;
+
+        public int score = 0;
+        public int hightScore = 0;
+
+        private List<GameObject> m_stones = new List<GameObject>(16);
+        private void Start()
+        {
+            m_lastSpawnedTime = Time.time;
+            RefreshDelay();
+        }
+        private void OnStickHit()
+        {
+            score++;
+            hightScore = Mathf.Max(hightScore, score);
+            Debug.Log($"Score: {score} - hightScore: {hightScore}");
+        }
+        private void OnEnable()
+        {
+            GameEvents.onStickHit += OnStickHit;
+            score = 0;
+        }
+
+        private void OnDisable()
+        {
+            GameEvents.onStickHit -= OnStickHit;
+        }
+
+        private void GameOver()
+        {
+            Debug.Log("Game Over!!!");
+            enabled = false;
+        }
+
+        public void ClearStones()
+        {
+            foreach (var stone in m_stones)
+            {
+                Destroy(stone);
+            }
+            m_stones.Clear();
+        }
+        public void RefreshDelay()
+        {
+            m_delay = UnityEngine.Random.Range(delayMin, delayMax);
+            delayMax = Mathf.Max(delayMin, delayMax - delayStep);
+        }
+        private void Update()
+        {
+                if (Time.time >= m_lastSpawnedTime + m_delay)
+                {
+                    var stone = spawner.Spawn();
+                    m_stones.Add(stone);
+                    m_lastSpawnedTime = Time.time;
+
+                    RefreshDelay();
+                }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
+
